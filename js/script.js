2b513236ebea242b30a9db5b4529b3c957bcbaf5
DarioDC95 <dario_95@live.com>
2022-11-29 20:49:50 +0100

    Bonus Exercise Done

--- js/script.js
+++ js/script.js
@@ -6,18 +6,23 @@
 //     2.4 - per quelli che non sono ne dell'uno, ne dell'altro.
 // 3 - Fare il console log nelle condizioni per stampare in console.
 
-let num = 1
+let quadrati = document.getElementById("boxes");
+let num = 1;
 for (let i = 1; i <= 100; i++) {
     if (i % 3 == 0 && i % 5 == 0) {
+        quadrati.innerHTML += `<div class="box box-red">FizzBuzz</div>`;
         console.log("FizzBuzz");
     }
     else if (i % 5 == 0) {
+        quadrati.innerHTML += `<div class="box box-yellow">Buzz</div>`;
         console.log("Buzz");
     }
     else if (i % 3 == 0) {
+        quadrati.innerHTML += `<div class="box box-green">Fizz</div>`;
         console.log("Fizz");
     }
     else {
+        quadrati.innerHTML += `<div class="box box-blue">${num}</div>`;
         console.log(num);
     };
     num += 1;
